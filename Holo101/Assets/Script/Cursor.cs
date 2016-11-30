bf8a2be17f4f5191522e47e45288d859eb6324c4
Laurent Bugnion <laurent@galasoft.ch>
2016-11-30 21:27:04 +0100

    Added Holo101 sample

---
+++ Holo101/Assets/Script/Cursor.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class Cursor : MonoBehaviour
+{
+    public GameObject TheCursor;
+    public Material InactiveCursorMaterial;
+    public Material ActiveCursorMaterial;
+    public Material ActiveFloatingCursorMaterial;
+
+    private GestureRecognizer _gestureRecognizer;
+    private RaycastHit _hitInfo;
+    private GameObject _focusedObject;
+
+    public static bool UseGravity
+    {
+        get;
+        set;
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        UseGravity = true;
+
+        _gestureRecognizer = new GestureRecognizer();
+        _gestureRecognizer.TappedEvent += GestureRecognizerOnTappedEvent;
+        _gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
+        _gestureRecognizer.StartCapturingGestures();
+    }
+
+    private void GestureRecognizerOnTappedEvent(InteractionSourceKind source, int tapcount, Ray headray)
+    {
+        if (_focusedObject == null)
+        {
+            return;
+        }
+
+        var rigidBody = _focusedObject.GetComponent<Rigidbody>();
+
+        if (rigidBody != null)
+        {
+            if (UseGravity)
+            {
+                if (rigidBody.useGravity)
+                {
+                    rigidBody.AddForce(-_hitInfo.normal * 100f);
+                }
+                else
+                {
+                    rigidBody.useGravity = true;
+                }
+            }
+            else
+            {
+                rigidBody.AddForce(-_hitInfo.normal * 100f);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+	    if (TheCursor == null)
+	    {
+	        return;
+	    }
+
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+        var cursorRenderer = TheCursor.GetComponent<Renderer>();
+
+        if (Physics.Raycast(headPosition, gazeDirection, out _hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            // But not the spatial mapping!
+            var obj = _hitInfo.collider.gameObject;
+
+            if (!obj.name.StartsWith("spatial")) // Fugly...
+            {
+                _focusedObject = obj;
+
+                if (UseGravity)
+                {
+                    cursorRenderer.material = ActiveCursorMaterial;
+                }
+                else
+                {
+                    cursorRenderer.material = ActiveFloatingCursorMaterial;
+                }
+            }
+            else
+            {
+                _focusedObject = null;
+                cursorRenderer.material = InactiveCursorMaterial;
+            }
+
+            TheCursor.transform.position = _hitInfo.point;
+            TheCursor.transform.forward = _hitInfo.normal;
+        }
+    }
+}
---
+++ Holo101/Assets/Script/SpeechManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    public  GameObject Container;
+    public Material FirstCubeMaterial;
+
+    private KeywordRecognizer _keywordRecognizer;
+    private readonly Dictionary<string, Action> _keywords = new Dictionary<string, Action>();
+    private readonly System.Random _random = new System.Random();
+
+    // Use this for initialization
+    private void Start()
+    {
+        _keywords.Add("Add cube", AddCube);
+        _keywords.Add("Reset scene", ResetScene);
+        _keywords.Add("Gravity", ToggleGravity);
+
+        _keywordRecognizer = new KeywordRecognizer(_keywords.Keys.ToArray());
+        _keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    private void ToggleGravity()
+    {
+        Cursor.UseGravity = !Cursor.UseGravity;
+    }
+
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        Action keywordAction;
+        if (_keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    private void ResetScene()
+    {
+        if (Container == null)
+        {
+            return;
+        }
+
+        foreach (Transform t in Container.transform)
+        {
+            var cube = t.gameObject;
+            Destroy(cube);
+        }
+    }
+
+    private void AddCube()
+    {
+        if (Container == null
+            || FirstCubeMaterial == null)
+        {
+            return;
+        }
+
+        var newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        newCube.transform.SetParent(Container.transform, true);
+        newCube.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+
+        newCube.transform.rotation = Quaternion.Euler(
+            45f,
+            0,
+            45f);
+
+        newCube.transform.localPosition = new Vector3(0f, 0f, 0f);
+
+        var rigidBody = newCube.AddComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+
+        var material = new Material(FirstCubeMaterial.shader);
+
+        var color = new Color(
+            (float)_random.NextDouble(),
+            (float)_random.NextDouble(),
+            (float)_random.NextDouble());
+
+        material.SetColor("_Color", color);
+
+        var newRenderer = newCube.GetComponent<Renderer>();
+        newRenderer.material = material;
+    }
+}
